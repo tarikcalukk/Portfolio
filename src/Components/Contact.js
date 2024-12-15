850828afb561ab5bb63a19ecf8db836165f66456
tarikcalukk <tcaluk1@etf.unsa.ba>
2024-12-15 01:49:47 +0100

    Editing contacts info

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaFacebookF, FaEnvelope } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFacebookF, FaEnvelope, FaInstagram } from "react-icons/fa";
 
 export default function Contact() {
   const email = "tarik.caluk@gmail.com"; 
   const linkedinLink = "https://www.linkedin.com/in/tarik-caluk/"; 
   const githubLink = "https://github.com/tarikcalukk";
   const facebookLink = "https://www.facebook.com/tarik.caluk.5";
+  const instagramLink = "https://www.instagram.com/tarik.caluk/";
 
   return (
     <section id="contact" className="text-gray-400 bg-gray-900 body-font">
@@ -43,50 +44,24 @@ export default function Contact() {
           </a>
 
           <a
+            href={instagramLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group w-24 h-24 bg-gray-800 rounded-full flex items-center justify-center text-white hover:bg-gradient-to-r hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 transition-all duration-300"
+          >
+            <FaInstagram className="w-12 h-12 group-hover:scale-110 transform transition-all" />
+          </a>
+
+          <a
             href={`mailto:${email}?subject=Hello&body=I would like to connect with you`}
             className="group w-24 h-24 bg-gray-800 rounded-full flex items-center justify-center text-white hover:bg-blue-500 transition-all duration-300"
           >
             <FaEnvelope className="w-12 h-12 group-hover:scale-110 transform transition-all" />
           </a>
         </div>
-
-        <div className="w-full max-w-md mx-auto bg-gray-800 p-8 rounded-lg">
-          <h2 className="text-xl font-semibold text-white mb-6">Send me a message</h2>
-          <form
-            action={`mailto:${email}`}
-            method="post"
-            encType="multipart/form-data"
-            className="flex flex-col"
-          >
-            <label htmlFor="name" className="text-white mb-2">
-              Your Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="mb-4 p-3 text-gray-800 rounded"
-              required
-            />
-
-            <label htmlFor="message" className="text-white mb-2">
-              Your Message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              rows="5"
-              className="mb-4 p-3 text-gray-800 rounded"
-              required
-            ></textarea>
-
-            <button
-              type="submit"
-              className="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 transition-all duration-300"
-            >
-              Send Message
-            </button>
-          </form>
+        <div className="text-white font-bold text-2xl mb-8">
+        <p>Don't hesitate to contact me:</p>
+        <p className="mt-2 text-xl text-blue-400">{email}</p>
         </div>
       </div>
     </section>
